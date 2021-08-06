2ad2f7f8c777f51dd957a98713c073fdadc6c024
Thalis Silva <thalis.silva@zallpy.com>
2021-08-06 14:32:47 -0300

    feat: configuration react-router-dom

--- src/components/TopSide/index.tsx
+++ src/components/TopSide/index.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Container,TitleBox, ButtonNew } from './styles';
+import { Container, TitleBox, ButtonNew } from './styles';
+import {
+  Link
+} from "react-router-dom";
 
 const TopSide: React.FC = () => {
   return (
     <Container>
-        <TitleBox>
+      <TitleBox>
         <h1>Gerenciamento de Tarefas</h1>
         <span>Tenha o controle de suas tarefas em um único lugar</span>
-        </TitleBox>
-
+      </TitleBox>
+      <Link to="/newtask">
         <ButtonNew>
-            Novo
-        </ButtonNew>
 
+          Novo
+        </ButtonNew>
+      </Link>
 
     </Container>
 
